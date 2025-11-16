900fd5432075ea3d329d4e7cb740144e4f30fe7f
Oleksiy_Lishchuk <ejik.alex9294@gmail.com>
2025-11-16 17:40:39 +0200

    fix movie

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,4 +1,4 @@
-interface Movie {
+export interface Movie {
   id: number;
   poster_path: string;
   backdrop_path: string;
@@ -6,18 +6,4 @@ interface Movie {
   overview: string;
   release_date: string;
   vote_average: number;
-}
-
-interface SearchBarProps {
-  onSubmit: (query: string) => void;
-}
-
-interface MovieGridProps {
-  movies: Movie[];
-  onSelect: (movie: Movie) => void;
-}
-
-interface MovieModalProps {
-  movie: Movie;
-  onClose: () => void;
 }
